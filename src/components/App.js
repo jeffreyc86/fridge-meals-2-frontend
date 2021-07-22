8ef0914378927e68a3eb6abf53a25ff9821100b7
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-07-22 16:05:41 -0400

    navbar near complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,41 @@
 import '../stylesheets/App.css';
-import { Route, Switch } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setCurrentUser } from "../features/userSlice";
+import Home from './Home'
+import Navbar from './Navbar';
 
 
 function App() {
 
-  const dispatch = useDispatch(function)
+  const dispatch = useDispatch()
+
+  useEffect(
+    () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        fetch(`${process.env.REACT_APP_RAILS_URL}show`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+          .then((res) => res.json())
+          .then((user) => {
+            dispatch(setCurrentUser(user));
+          });
+      }
+    },
+    // eslint-disable-next-line
+    []
+  );
 
   return (
     <div className="App">
       <Switch>
-        <Route path ="/">
-          Home
+        <Navbar />
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,82 @@
+import "../stylesheets/Navbar.css";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+
+function Navbar() {
+  const currentUser = useSelector((state) => state.user.currentUser);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className="navbar">
+      <img href={null} alt="logo" />
+      {!currentUser ? (
+        <div className="nav-links">
+          <NavLink to="" className="nav-center-links">
+            Recipes you can make
+          </NavLink>
+          <NavLink to="" className="nav-center-links">
+            Search Recipes
+          </NavLink>
+          <NavLink to="/" className="nav-center-links">
+            Home
+          </NavLink>
+        </div>
+      ) : null}
+      <div className="nav-dropdwn">
+        <Button
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <AccountCircleIcon color="primary" />
+        </Button>
+        <Menu
+          elevation={0}
+          getContentAnchorEl={null}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+          className="user-menu"
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          {currentUser ? (
+            <>
+              <MenuItem>Profile</MenuItem>
+              <MenuItem>Log Out</MenuItem>
+            </>
+          ) : (
+            <>
+              <MenuItem>Sign Up</MenuItem>
+              <MenuItem>Log In</MenuItem>
+            </>
+          )}
+        </Menu>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
